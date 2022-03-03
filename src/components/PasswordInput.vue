c66b30555dbf012496923462e4ef33975d574502
Kamila Lach <kamila.lach@monterail.com>
2022-03-03 14:00:48 +0100

    Add password visibility toggler

--- src/components/PasswordInput.vue
+++ src/components/PasswordInput.vue
@@ -1,19 +1,27 @@
 <template>
   <label class="password-input">
     Password
-    <input
-      class="password-input__input"
-      type="password"
-      id="password"
-      name="password"
-      placeholder="Enter your password"
-    />
-    <button class="buttontest">
-      <img src="@/assets/icons/view.svg" alt="show password" />
-    </button>
-    <button>
-      <img src="@/assets/icons/view-off.svg" alt="hide password" />
-    </button>
+    <div class="password-input__wrapper">
+      <input
+        class="password-input__input"
+        :type="type"
+        id="password"
+        name="password"
+        placeholder="Enter your password"
+      />
+      <button class="password-input__button" @click="showPassword">
+        <img
+          v-show="passwordHidden"
+          src="@/assets/icons/view.svg"
+          alt="show password"
+        />
+        <img
+          v-show="!passwordHidden"
+          src="@/assets/icons/view-off.svg"
+          alt="hide password"
+        />
+      </button>
+    </div>
   </label>
 </template>
 
@@ -21,20 +29,53 @@
 export default {
   name: "PasswordInput",
   components: {},
+  data() {
+    return {
+      type: "password",
+    };
+  },
+  computed: {
+    passwordHidden() {
+      return this.type === "password";
+    },
+  },
+  methods: {
+    showPassword() {
+      if (this.type === "password") {
+        this.type = "text";
+      } else {
+        this.type = "password";
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.buttontest {
-  height: 50px;
-  width: 50px;
-}
 .password-input {
   @include font-element--small-accent;
+
+  &__wrapper {
+    display: flex;
+    position: relative;
+  }
 
   &__input {
     @include input-element;
     display: block;
+  }
+
+  &__button {
+    background: transparent;
+    border: none;
+    outline: none;
+    display: block;
+    cursor: pointer;
+    position: absolute;
+    transform: translate(0, -50%);
+    z-index: 2;
+    right: 8px;
+    top: 50%;
   }
 }
 </style>
