653693aa58c91acc05ffa3f022054c1584351236
Kamila Lach <kamila.lach@monterail.com>
2022-03-04 13:49:59 +0100

    CR changes

--- src/api/movies.js
+++ src/api/movies.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const API_BASE_URL = "http://localhost:3000";
 
+export const MOVIES_URL = "movies";
+
 export const getMovies = async () => {
-  return await axios.get(`${API_BASE_URL}/movies`);
+  return await axios.get(`${API_BASE_URL}/${MOVIES_URL}`);
 };
