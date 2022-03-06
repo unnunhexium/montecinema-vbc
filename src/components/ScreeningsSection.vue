d64615569aa647ed035cf273d7589cee67f691f2
Kamila Lach <kamila.lach@monterail.com>
2022-03-06 21:17:34 +0100

    Add functionality to display weekday and date

--- src/components/ScreeningsSection.vue
+++ src/components/ScreeningsSection.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="screening-section">
-    <h2 class="screening-section__title">Screenings:</h2>
-    <p class="screening-section__content">Friday 11/02/2022</p>
-    <div class="screening-section__input-wrapper">
-      <DayTabsList></DayTabsList>
-      <MovieSelect></MovieSelect>
+  <div class="screenings-section">
+    <h2 class="screenings-section__title">Screenings:</h2>
+    <p class="screenings-section__content">{{ getDayOfWeek }} {{ getDate }}</p>
+    <div class="screenings-section__input-wrapper">
+      <DayTabsList />
+      <MovieSelect />
+    </div>
+    <div class="screenings-section__cards-wrapper">
+      <ScreeningCard v-for="movie in movies" :movie="movie" :key="movie.id" />
     </div>
   </div>
 </template>
@@ -12,27 +15,60 @@
 <script>
 import DayTabsList from "./DayTabsList.vue";
 import MovieSelect from "./base/MovieSelect.vue";
-
+import ScreeningCard from "./ScreeningCard.vue";
 export default {
-  components: { DayTabsList, MovieSelect },
+  components: { DayTabsList, MovieSelect, ScreeningCard },
+  computed: {
+    movies() {
+      return this.$store.state.movies;
+    },
+    imageAlt() {
+      return `An image from ${this.movie.title} film.`;
+    },
+    getDayOfWeek() {
+      const dayOfWeek = new Date().getDay();
+      return isNaN(dayOfWeek)
+        ? null
+        : [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+          ][dayOfWeek];
+    },
+    getDate() {
+      const today = new Date();
+      const yyyy = today.getFullYear();
+      const mm = `0${today.getMonth() + 1}`.slice(-2);
+      const dd = `0${today.getDate()}`.slice(-2);
+      return `${dd}/${mm}/${yyyy}`;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.screening-section {
+.screenings-section {
   padding: 4em 3em 0;
+
   &__title,
   &__content {
     @include font-heading--big;
   }
+
   &__content {
     color: $text-lighter;
     padding-bottom: 0.25em;
     display: flex;
   }
+
   &__input-wrapper {
     display: flex;
     justify-content: space-between;
+    padding-bottom: 6.125em;
   }
 }
 </style>
