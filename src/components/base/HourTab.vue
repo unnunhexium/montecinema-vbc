2d7f13442cf275e3beef5e156b401633c81a802e
Kamila Lach <kamila.lach@monterail.com>
2022-03-06 21:11:32 +0100

    Create HourTab component

--- src/components/base/HourTab.vue
+++ src/components/base/HourTab.vue
@@ -1,10 +1,24 @@
 <template>
-  <div></div>
+  <div class="hour-tab"><slot /></div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "HourTab",
+};
 </script>
 
 <style lang="scss" scoped>
+.hour-tab {
+  @include time-pill;
+  font-size: 16px;
+  color: $text-accent;
+  padding: 0.75rem 2rem;
+  border: 2px solid $btn-default;
+  &:active,
+  &:focus,
+  &:hover {
+    background: $btn-default;
+  }
+}
 </style>
