f3e716f46326c28213f4510e61cd10c0a954e915
Kamila Lach <kamila.lach@monterail.com>
2022-03-08 16:19:55 +0100

    Add AllMoviesPage

--- src/components/pages/public/AllMoviesPage.vue
+++ src/components/pages/public/AllMoviesPage.vue
@@ -1,11 +1,88 @@
 <template>
-  <div>This is all movies page.</div>
+  <div class="all-movies-page">
+    <BreadcrumbNav class="all-movies-page__breadcrumb-nav" />
+    <h2 class="all-movies-page__heading">All the movies</h2>
+    <div class="all-movies-page__wrapper">
+      <div class="all-movies-page__search-wrapper">
+        <BaseInput
+          v-model="query"
+          class="all-movies-page__search-input"
+          type="search"
+          placeholder="What are you looking for?"
+          label="search"
+        />
+      </div>
+      <MovieSelect />
+    </div>
+    <div class="all-movies-page__movies-list">
+      <MovieCard v-for="movie in movies" :movie="movie" :key="movie.id" />
+    </div>
+  </div>
 </template>
 
 <script>
+import BreadcrumbNav from "@/components/BreadcrumbNav.vue";
+import BaseInput from "@/components/base/BaseInput.vue";
+import MovieSelect from "@/components/base/MovieSelect.vue";
+import MovieCard from "@/components/MovieCard.vue";
+
 export default {
   name: "AllMoviesPage",
+  components: { BreadcrumbNav, BaseInput, MovieSelect, MovieCard },
+  data() {
+    return {
+      query: "",
+    };
+  },
+  computed: {
+    movies() {
+      return this.$store.state.movies;
+    },
+  },
+  async created() {
+    this.$store.dispatch("fetchMovies");
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.all-movies-page {
+  padding: 0 48px;
+  &__breadcrumb-nav {
+    margin-bottom: 4em;
+  }
+
+  &__heading {
+    @include font-heading--large;
+    margin-bottom: 0.4em;
+  }
+
+  &__wrapper {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    grid-gap: 2.25em;
+  }
+
+  &__search-wrapper {
+    display: flex;
+    position: relative;
+  }
+
+  &__search-input {
+  }
+
+  &__movies-list {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 40px;
+    padding-bottom: 64px;
+    @include lg {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @include md {
+      grid-template-columns: 1fr;
+      padding-bottom: 9em;
+    }
+  }
+}
+</style>
