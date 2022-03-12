75c0a3e00557c269b64b4e9c78792d8ac48bcc76
Kamila Lach <kamila.lach@monterail.com>
2022-03-12 11:42:05 +0100

    Move logic to the outside of the component

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,3 +7,29 @@ export const getFormattedLength = (length) => {
   const hours = (length - minutes) / 60;
   return `${hours}h ${minutes}min`;
 };
+
+export const getYear = (string) => {
+  return string.slice(0, 4);
+};
+
+export const getDayOfWeek = () => {
+  const dayOfWeek = new Date().getDay();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[dayOfWeek];
+};
+
+export const getDate = () => {
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = `0${today.getMonth() + 1}`.slice(-2);
+  const dd = `0${today.getDate()}`.slice(-2);
+  return `${dd}/${mm}/${yyyy}`;
+};
