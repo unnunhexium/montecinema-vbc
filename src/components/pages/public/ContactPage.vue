adb56f899e1db8243bf90cc49c35908ae78917f5
Kamila Lach <kamila.lach@monterail.com>
2022-03-15 22:15:32 +0100

    Add alt attributes to icons and image

--- src/components/pages/public/ContactPage.vue
+++ src/components/pages/public/ContactPage.vue
@@ -2,18 +2,22 @@
   <div class="contact-page">
     <h2 class="contact-page__heading">Contact us</h2>
     <div class="contact-page__content-wrapper">
-      <img src="@/assets/images/map.jpg" class="contact-page__map" />
+      <img
+        src="@/assets/images/map.jpg"
+        class="contact-page__map"
+        alt="Cinema location on the map."
+      />
       <div class="contact-page__contact-card">
         <ContactCard />
       </div>
       <div class="contact-page__media-icons">
-        <img src=@/assets/icons/media/twitter.svg/> <img
-        src=@/assets/icons/media/facebook.svg/> <img
-        src=@/assets/icons/media/linkedin.svg/> <img
-        src=@/assets/icons/media/instagram.svg/> <img
-        src=@/assets/icons/media/github.svg/> <img
-        src=@/assets/icons/media/behance.svg/><img
-        src=@/assets/icons/media/basketball.svg/>
+        <img src=@/assets/icons/media/twitter.svg/ alt="Logo of Twitter."> <img
+        src=@/assets/icons/media/facebook.svg/ alt="Logo of Facebook."> <img
+        src=@/assets/icons/media/linkedin.svg/ alt="Logo of LinkedIn."> <img
+        src=@/assets/icons/media/instagram.svg/ alt="Logo of Instagram."> <img
+        src=@/assets/icons/media/github.svg/ alt="Logo of GitHub."> <img
+        src=@/assets/icons/media/behance.svg/ alt="Logo of Behance."><img
+        src=@/assets/icons/media/basketball.svg alt="Logo of Basketball."/>
       </div>
     </div>
   </div>
