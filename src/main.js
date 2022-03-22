f9f3f583466a1b2e2a74f8c529861558ecda1a7d
Kamila Lach <kamila.lach@monterail.com>
2022-03-22 14:52:18 +0100

    Install Vue Meta

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import Vue from "vue";
 import router from "@/router";
 import VueRouter from "vue-router";
+import Meta from "vue-meta";
+import store from "./store";
 import App from "./App.vue";
-import store from './store'
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+Vue.use(Meta);
 
 new Vue({
   router,
   store,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
