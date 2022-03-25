8f76dedf1d0d6a3098a4a773d68c0418ed9fa0ac
Kamila Lach <kamila.lach@monterail.com>
2022-03-25 16:37:38 +0100

    Add SeatsList

--- src/components/pages/client/ChooseSeatsAndTicketsPage.vue
+++ src/components/pages/client/ChooseSeatsAndTicketsPage.vue
@@ -7,19 +7,36 @@
       :movie="selectedMovie.movie"
       :datetime="selectedMovie.datetime"
     />
+    <div>
+      <SeatsList
+        class="choose-seats-and-tickets-page__seats"
+        :hallId="selectedMovie.hall"
+      />
+      <BaseButton type="tertiary" class="choose-seats-and-tickets-page__button">
+        <!-- TO DO v-if seats are chosen -->
+        Choose <slot>4</slot> seats
+      </BaseButton>
+    </div>
+    <div><ChooseTickets /></div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import StepsNav from "@/components/navigation/StepsNav.vue";
 import ScreeningCard from "@/components/ScreeningCard.vue";
-import { mapGetters } from "vuex";
+import SeatsList from "@/components/SeatsList.vue";
+import ChooseTickets from "@/components/ChooseTickets.vue";
+import BaseButton from "@/components/base/BaseButton.vue";
 
 export default {
   name: "ChooseSeatsAndTicketsPage",
   components: {
     StepsNav,
     ScreeningCard,
+    SeatsList,
+    ChooseTickets,
+    BaseButton,
   },
   computed: {
     ...mapGetters(["selectedMovie"]),
@@ -29,7 +46,7 @@ export default {
 
 <style lang="scss" scoped>
 .choose-seats-and-tickets-page {
-  padding: 0 48px;
+  padding: 0 48px 64px;
 
   &__nav {
     margin-bottom: 4em;
@@ -47,6 +64,22 @@ export default {
     box-shadow: 0px 24px 78px rgba(0, 0, 0, 0.08),
       0px 5.36071px 17.4223px rgba(0, 0, 0, 0.0238443),
       0px 1.59602px 5.18708px rgba(0, 0, 0, 0.0161557);
+  }
+
+  &__seats {
+    margin-bottom: 4em;
+  }
+
+  &__button {
+    color: #f7a0a1;
+    border: 2px solid #f7a0a1;
+    display: block;
+    margin-left: auto;
+    &:active,
+    &:focus,
+    &:hover {
+      background: #f7a0a1;
+    }
   }
 }
 </style>
