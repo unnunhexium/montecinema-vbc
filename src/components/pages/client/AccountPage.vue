253519c4bcfb2dc0924f7aa7a1e08abde68ca96f
Kamila Lach <kamila.lach@monterail.com>
2022-04-22 13:18:10 +0200

    Create AccountPage

---
+++ src/components/pages/client/AccountPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="account-page">
+    <div class="account-page__wrapper">
+      <h2 class="account-page__title">Personal details</h2>
+      <h2 class="account-page__title">Reservations</h2>
+    </div>
+    <PersonalDetailsCard></PersonalDetailsCard>
+  </div>
+</template>
+
+<script>
+import PersonalDetailsCard from "@/components/PersonalDetailsCard.vue";
+export default {
+  name: "AccountPage",
+  components: { PersonalDetailsCard },
+};
+</script>
+
+<style lang="scss" scoped>
+.account-page {
+  padding: 0 48px;
+  margin-top: 0.75em;
+  &__wrapper {
+    display: flex;
+  }
+  &__title {
+    font-family: "Eczar";
+    font-weight: 600;
+    font-size: 48px;
+    line-height: 49px;
+    margin: 0 0.75em 1.35em 0;
+  }
+}
+</style>
