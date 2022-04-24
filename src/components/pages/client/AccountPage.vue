3434b346f876ac668b94f930d957cc881d9192e8
Kamila Lach <kamila.lach@monterail.com>
2022-04-24 23:27:11 +0200

    Add nav buttons, add ReservationsCard

--- src/components/pages/client/AccountPage.vue
+++ src/components/pages/client/AccountPage.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="account-page">
     <div class="account-page__wrapper">
-      <h2 class="account-page__title">Personal details</h2>
-      <h2 class="account-page__title">Reservations</h2>
+      <button
+        :class="[
+          'account-page__title',
+          { 'account-page__title--active': !secondStepActive },
+        ]"
+        @click="secondStepActive = false"
+      >
+        Personal details
+      </button>
+      <button
+        :class="[
+          'account-page__title',
+          { 'account-page__title--active': secondStepActive },
+        ]"
+        @click="secondStepActive = true"
+      >
+        Reservations
+      </button>
     </div>
-    <PersonalDetailsCard></PersonalDetailsCard>
+    <PersonalDetailsCard
+      :formData="formData"
+      @set-form-field="setFormField"
+      v-if="!secondStepActive"
+    />
+    <ReservationsCard v-else />
   </div>
 </template>
 
 <script>
 import PersonalDetailsCard from "@/components/PersonalDetailsCard.vue";
+import ReservationsCard from "@/components/ReservationsCard.vue";
+import { mapGetters } from "vuex";
+
 export default {
   name: "AccountPage",
-  components: { PersonalDetailsCard },
+  components: { PersonalDetailsCard, ReservationsCard },
+  data() {
+    return {
+      secondStepActive: false,
+      formData: {
+        email: "",
+        firstName: "",
+        lastName: "",
+        dateOfBirth: "",
+      },
+    };
+  },
+  methods: {
+    setFormField({ value, key }) {
+      this.formData[key] = value;
+    },
+  },
+  computed: {
+    ...mapGetters(["userData"]),
+  },
+  mounted() {
+    this.formData.email = this.userData.email;
+    this.formData.firstName = this.userData.first_name;
+    this.formData.lastName = this.userData.last_name;
+    this.formData.dateOfBirth = this.userData.date_of_birth;
+  },
 };
 </script>
 
@@ -20,15 +69,32 @@ export default {
 .account-page {
   padding: 0 48px;
   margin-top: 64px;
+
   &__wrapper {
     display: flex;
   }
+
   &__title {
+    all: unset;
+    cursor: pointer;
+    color: $text-lighter;
     font-family: "Eczar";
     font-weight: 600;
     font-size: 48px;
     line-height: 49px;
     margin: 0 0.75em 1.35em 0;
+
+    &--active {
+      color: $text-dark;
+    }
+
+    &--inactive {
+      color: $text-lighter;
+    }
+
+    &:focus-visible {
+      color: $text-accent;
+    }
   }
 }
 </style>
