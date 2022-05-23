c9e77420cbf95a8af579e50306371a756d0ae80e
Kamila Lach <kamila.lach@monterail.com>
2022-05-23 08:34:30 +0200

    Create CreateAReservationPage

---
+++ src/components/pages/employee/CreateAReservationPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><ScreeningsSection /></div>
+</template>
+
+<script>
+import ScreeningsSection from "@/components/ScreeningsSection.vue";
+export default {
+  name: "CreateAReservationPage",
+  components: {
+    ScreeningsSection,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
